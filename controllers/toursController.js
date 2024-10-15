67809b89d1f3394bf5125cdcba3442a601365681
Chris felicien <f.ilungakalenga@gmail.com>
2024-10-15 10:08:07 +0200

    Adding the slug to model, add some model middlare and add some string validator to the model

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -128,7 +128,7 @@ export const createTour = async (req, res) => {
   } catch (error) {
     res.status(400).json({
       status: 'fail',
-      message: `Invalid data sent`,
+      message: error.message,
     });
   }
 };
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import slugify from 'slugify';
 
 const tourSchema = new mongoose.Schema(
   {
@@ -7,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'Please provide the name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'Tour name must be less or equal to 40'],
+      minlength: [7, 'Tour name must be more or equal to 7'],
     },
     price: { type: Number, required: [true, 'Please provide the price'] },
     duration: {
@@ -21,10 +24,16 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, 'Tour must have difficulty'],
       trim: true,
+      enum: {
+        values: ['difficult', 'medium', 'easy'],
+        message: `Please provide valid easy, medium or difficult`,
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be bellow 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -58,12 +67,39 @@ const tourSchema = new mongoose.Schema(
     startDates: {
       type: [Date],
     },
+    slug: {
+      type: String,
+    },
+    secret: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
 tourSchema.virtual('durationWeek').get(function () {
   return this.duration / 7;
+});
+
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secret: { $ne: true } });
+  next();
+});
+
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline?.().unshift({
+    $match: {
+      secret: { $ne: true },
+    },
+  });
+
+  next();
 });
 
 export default mongoose.model('Tour', tourSchema);
