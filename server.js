5379f3426ee62a19ce02d7ee04c771b121a2d20c
Chris felicien <f.ilungakalenga@gmail.com>
2024-10-16 16:35:02 +0200

    Catch error on async and sync code

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import {} from 'dotenv/config';
 import mongoose from 'mongoose';
 import app from './app.js';
+process.on('uncaughtException', (err) => {
+  console.log(err);
+  console.log('Shouting down the server');
+
+  process.exit(1);
+});
 
 const PORT = process.env.PORT || 4000;
 
@@ -8,6 +14,13 @@ const DB = process.env.MONGO_URL.replace(/PASSWORD/, process.env.DB_PASSWORD);
 mongoose
   .connect(DB)
   .then(() => console.log('Connected to DB'))
-  .catch((err) => console.log(`Something went wrong while  connecting to DB`));
+  .catch((err) => console.log(err));
 
-app.listen(PORT, () => console.log(`The app is running on port ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`The app is running on port ${PORT}`)
+);
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  server.close(() => process.exit(1));
+});
