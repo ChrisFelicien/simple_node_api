37f8a531f9f2be7500ad67672249b5011dfc944e
Chris felicien <f.ilungakalenga@gmail.com>
2024-10-18 10:12:15 +0200

    Create a token with jwt and send it to the user

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,19 +1,24 @@
+import jwt from 'jsonwebtoken';
 import User from './../models/userModel.js';
 import asyncWrapper from './asyncWrapper.js';
 
 export const signup = asyncWrapper(async (req, res, next) => {
-  const { name, email, password, passwordConfirm, photo } = req.body;
+  const { name, email, password, passwordConfirm } = req.body;
 
   const newUser = await User.create({
     name,
     email,
     password,
     passwordConfirm,
-    photo,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE_IN,
   });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
